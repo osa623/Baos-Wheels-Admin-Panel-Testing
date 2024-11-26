76153ac3b63e1399b7087e32b2a0205d0fa60917
osa623 <it22170798@my.sliit.lk>
2024-11-26 15:50:32 +0530

    commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
